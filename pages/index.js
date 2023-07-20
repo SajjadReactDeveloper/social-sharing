ad369d7db2cc56b7f4d06e0388f362d941df3a09
Sajjad Akhtar <sajjad.akhtar@funavry.com>
2023-07-20 16:02:32 +0500

    Hello

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ export default function Home() {
         <title>
           Social Media Preview
         </title>
-        <meta property="og:url" content="your url" />
+        <meta property="og:url" content="https://social-sharing-10p3k0mvn-sajjadreactdeveloper.vercel.app/" />
         <meta property="og:type" content="website" />
         <meta
           property="og:title"
@@ -19,7 +19,7 @@ export default function Home() {
           property="og:description"
           content="Hurray!! Yes Social Media Preview is Working"
         />
-        <meta property="og:image" content={"url of image"} />
+        <meta property="og:image" content={"https://styles/assetsOptimized/ourStory/StoreFront.jpeg"} />
       </Head>
       <h2>
         Hurray!!! The Website is Complete. Lets Try Social Media Preview Now
